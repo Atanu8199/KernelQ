66753ae720cdbd25bce0649fac1af971e7c10fcc
TeraaBytee <terabyte3766@gmail.com>
2021-11-28 22:52:38 +0700

    drivers/misc: mtk-ram_console: Fix warning address always eveluate as true
    Signed-off-by: TeraaBytee <terabyte3766@gmail.com>

--- drivers/misc/mediatek/ram_console/mtk_ram_console.c
+++ drivers/misc/mediatek/ram_console/mtk_ram_console.c
@@ -3382,10 +3382,8 @@ void aee_rr_show_last_bus(struct seq_file *m)
 	char *reg_buf = kmalloc(4096, GFP_KERNEL);
 
 	if (reg_buf) {
-		if (mt_lastbus_dump) {
-			if (mt_lastbus_dump(reg_buf) == 0)
-				seq_printf(m, "%s\n", reg_buf);
-		}
+		if (mt_lastbus_dump(reg_buf) == 0)
+			seq_printf(m, "%s\n", reg_buf);
 		kfree(reg_buf);
 	}
 }

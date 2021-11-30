dab18f2d806aae08e0a98a80ccb6cae1807d6e9b
TeraaBytee <terabyte3766@gmail.com>
2021-11-30 11:11:45 +0700

    kernel: sched: Remove comparison will always evaluate as false
    Signed-off-by: TeraaBytee <terabyte3766@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7754,8 +7754,6 @@ void __init sched_init(void)
 
 #ifdef CONFIG_SMP
 	/* May be allocated at isolcpus cmdline parse time */
-	if (cpu_isolated_map == NULL)
-		zalloc_cpumask_var(&cpu_isolated_map, GFP_NOWAIT);
 	idle_thread_set_boot_cpu();
 	set_cpu_rq_start_time(smp_processor_id());
 #endif
